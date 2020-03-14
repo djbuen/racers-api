2a37305851f1a9e2f7d319084c2d8074e6108451
dave buenconsejo <daveb@ekconsulting.net>
2020-03-15 01:44:06 +0800

    updates for racer model
      * added racer model to save storage

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,2 +1,38 @@
-class Racer < ApplicationRecord
+class Racer
+  # Add Active Model support.
+  # Provides constructor that takes a Hash of attribute values.
+  include ActiveModel::Model
+
+  # Add Active Model validation support to Book class.
+  include ActiveModel::Validations
+
+  validates :first_name, :last_name, presence: true
+
+  attr_accessor :racer_id, :bid_number, :first_name, :last_name
+
+  # Return a Google::Cloud::Firestore::Dataset for the configured collection.
+  # The collection is used to create, read, update, and delete entity objects.
+  def self.collection
+    project_id = ENV["GOOGLE_CLOUD_PROJECT"]
+    raise "Set the GOOGLE_CLOUD_PROJECT environment variable" if project_id.nil?
+
+    require "google/cloud/firestore"
+    firestore = Google::Cloud::Firestore.new project_id: project_id
+    @collection = firestore.col "racers"
+  end
+
+ def save
+    if valid?
+      racer_ref = Racer.collection.doc racer_id
+      racer_id = Racer.collection.get().count + 1
+      racer_ref.set \
+        first_name: first_name,
+        last_name:  last_name,
+        racer_id:   racer_ref.document_id,
+        bid_number: racer_id     
+      true
+    else
+      false
+    end
+  end
 end
