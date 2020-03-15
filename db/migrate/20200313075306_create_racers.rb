76f95aa6b55905378d5bde75675336b375df0175
dave buenconsejo <daveb@ekconsulting.net>
2020-03-15 23:19:06 +0800

    comment migration for now

--- db/migrate/20200313075306_create_racers.rb
+++ db/migrate/20200313075306_create_racers.rb
@@ -1,12 +1,12 @@
 class CreateRacers < ActiveRecord::Migration[6.0]
   def change
-    create_table :racers do |t|
-      t.string :racer_id
-      t.int :bid_number
-      t.string :first_name
-      t.string :last_name
+ #   create_table :racers do |t|
+ #     t.string :racer_id
+ #     t.int :bid_number
+ #     t.string :first_name
+ #     t.string :last_name
 
-      t.timestamps
-    end
+ #     t.timestamps
+ #   end
   end
 end
